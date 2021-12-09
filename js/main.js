bcd819613823349cf4e55f60f5a4adc57649eac4
00012387 <wiut00012387@gmail.com>
2021-12-09 19:27:37 +0500

    js file improved

--- js/main.js
+++ js/main.js
@@ -2,15 +2,13 @@ var elOpenModalButton = document.querySelector('.js-modal-open');
 var elModal = document.querySelector('.js-modal');
 var closeButton = document.querySelector('.exit-button');
 
-elOpenModalButton.addEventListener('click', function ()  {
-    elModal.classList.add('modal-open')
+elOpenModalButton.addEventListener('click', function () {
+	elModal.classList.add('modal-open');
 });
 
-closeButton.addEventListener('click', function(){
-    elModal.classList.remove('modal-open')
+closeButton.addEventListener('click', function () {
+	elModal.classList.remove('modal-open');
 });
-
-
 
 // FAQ section js
 
@@ -18,17 +16,17 @@ elsqaItem = document.querySelectorAll('.faq-list__item');
 elsqaToggler = document.querySelectorAll('.faq-list__item__btn');
 
 elsqaToggler.forEach(function (toggler) {
-  toggler.addEventListener('click',function () {
-      toggler.closest('.faq-list__item').classList.toggle('faq--open');
-  })
-})
+	toggler.addEventListener('click', function () {
+		toggler.closest('.faq-list__item').classList.toggle('faq--open');
+	});
+});
 
 // Hamburger code
 
-const elHamburger = document.querySelector('.header__menu-button')
-const elHeader  =  document.querySelector('.header')
+const elHamburger = document.querySelector('.header__menu-button');
+const elHeader = document.querySelector('.header');
 
 elHamburger.addEventListener('click', () => {
-    console.log("Pressed");
-    elHeader.classList.toggle ('header--active')
-})
+	console.log('Pressed');
+	elHeader.classList.toggle('header--active');
+});
