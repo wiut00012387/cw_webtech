732d096ae431892fdb4522b3dfbf4365a962d333
00012387 <wiut00012387@gmail.com>
2021-12-09 07:53:06 +0500

    Js file uploaded

---
+++ js/main.js
@@ -0,0 +1,33 @@
+var elOpenModalButton = document.querySelector('.js-modal-open');
+var elModal = document.querySelector('.js-modal');
+var closeButton = document.querySelector('.exit-button');
+
+elOpenModalButton.addEventListener('click', function ()  {
+    elModal.classList.add('modal-open')
+});
+
+closeButton.addEventListener('click', function(){
+    elModal.classList.remove('modal-open')
+});
+
+
+
+// FAQ
+
+elsqaItem = document.querySelectorAll('.faq-list__item');
+elsqaToggler = document.querySelectorAll('.faq-list__item__btn');
+
+elsqaToggler.forEach(function (toggler) {
+  toggler.addEventListener('click',function () {
+      toggler.closest('.faq-list__item').classList.toggle('faq--open');
+  })
+})
+
+/*
+const elHamburger = document.querySelector('.header__menu-button')
+const elHeader  =  document.querySelector('.header')
+
+elHamburger.addEventListener('click', () =>) {
+    elHeader.classList.toggle ('header--active')
+}
+*/
